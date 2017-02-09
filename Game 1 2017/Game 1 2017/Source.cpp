b127f99a110455cef8d1e123a33582e7344a0f38
Elias <ezwhalen64@gmaikl.com>
2017-02-09 12:26:02 -0700

    khgv

--- Game 1 2017/Game 1 2017/Source.cpp
+++ Game 1 2017/Game 1 2017/Source.cpp
@@ -5,7 +5,6 @@
 #include "allegro5/allegro.h"
 #include "allegro5/allegro_image.h"
 using namespace std;
-void Monster();//Declaring function called monster
 int main(int argc, char **argv) {
 	al_init();
 	al_init_image_addon();
@@ -52,7 +51,7 @@ int main(int argc, char **argv) {
 			al_destroy_bitmap(picture1);
 		case 2:system("Color E0");
 			picture2 = al_load_bitmap("Hall.jpg");
-			al_draw_bitmap(picture2, 50, 200, 0);
+			al_draw_bitmap(picture2, 0, 200, 0);
 			al_flip_display();
 			cout << "You are in a hallway." << endl;
 			if (inv[1].compare("hoodie") != 0)
@@ -70,9 +69,8 @@ int main(int argc, char **argv) {
 			break;
 		case 3:system("Color FB");
 			picture3 = al_load_bitmap("Bathroom.jpg");
-			al_draw_bitmap(picture3, 25, 50, 0);
+			al_draw_bitmap(picture3, 0, 50, 0);
 			al_flip_display();
-			al_destroy_bitmap(picture3);
 			cout << "You are in a bathroom. It is a white porcelain room with a normal looking toilet and sink." << endl;
 			if (inv[2].compare("hairbrush") != 0)
 				cout << "There is a hairbrush on the toilet" << endl;
@@ -89,9 +87,8 @@ int main(int argc, char **argv) {
 			break;
 		case 4:system("Color 4F");
 			picture4 = al_load_bitmap("bar.jpg");
-			al_draw_bitmap(picture4, 500, 200, 0);
+			al_draw_bitmap(picture4, -20, 50, 0);
 			al_flip_display();
-			al_destroy_bitmap(picture4);
 			cout << "You are in a cantina. It is a big room with aliens all over and a band playing a song on repeat.(Haven't you seen Star Wars?) There is a bartender behind the counter." << endl;
 			if (inv[3].compare("bottle") != 0)
 				cout << "There is a bottle of alcohol on the table." << endl;
@@ -110,10 +107,8 @@ int main(int argc, char **argv) {
 			break;
 		case 5: system("Color 20");
 			picture5 = al_load_bitmap("game room.jpg");
-			al_draw_bitmap(picture5, 500, 200, 0);
+			al_draw_bitmap(picture5, -20, 50, 0);
 			al_flip_display();
-			Monster();
-			al_destroy_bitmap(picture5);
 			cout << "You are in a game room. There are scattered arcade games, old and new. There is a large pool table in the center of the room." << endl;
 			if (inv[4].compare("sword") != 0)
 				cout << "There is a sword underneath the pool table" << endl;
@@ -130,12 +125,10 @@ int main(int argc, char **argv) {
 			break;
 		case 6:system("Color 5F");
 			picture6 = al_load_bitmap("turret.jpg");
-			al_draw_bitmap(picture6, 500, 200, 0);
+			al_draw_bitmap(picture6, 0, 50, 0);
 			al_flip_display();
-			al_destroy_bitmap(picture6);
-			Monster();
 			cout << "You are in the gun room. There is a huge gun with the barrel pointed toward the window with the barrel of the gun outside of the ship." << endl;
-			cout << "You can go South(s), or East(e)." << endl;
+			cout << "You can go East(e)." << endl;
 			getline(cin, input);
 			if (input.compare("north") == 0 && inv[4].compare("sword") == 0)
 				room = 10;
@@ -144,10 +137,8 @@ int main(int argc, char **argv) {
 			break;
 		case 7: system("Color 0F");
 			picture7 = al_load_bitmap("control room.jpg");
-			al_draw_bitmap(picture7, 500, 200, 0);
+			al_draw_bitmap(picture7, 0, 50, 0);
 			al_flip_display();
-			al_destroy_bitmap(picture7);
-			Monster();
 			cout << "You are in the flight deck of the ship. It is a fairly big room with nothing but an empty chair and a huge panel of controls in front of it. Outside of the window, you see a distant, alien looking planet, and nothing else but stars." << endl;
 			cout << "You can go South(s), East(e), or West(w)." << endl;
 			getline(cin, input);
@@ -160,13 +151,11 @@ int main(int argc, char **argv) {
 			break;
 		case 8:system("Color 1A");
 			picture8 = al_load_bitmap("Armory.jpg");
-			al_draw_bitmap(picture8, 500, 200, 0);
+			al_draw_bitmap(picture8, 0, 50, 0);
 			al_flip_display();
-			al_destroy_bitmap(picture8);
-			Monster();
 			cout << "You are in an armory. There is nothing here but big black crates." << endl;
 			if (inv[5].compare("gun") != 0)
-				cout << "The crates are filled with alien guns." << endl;
+				cout << "The crates are filled with Earth guns." << endl;
 			cout << "You can go North(n),West(w), or South(s)." << endl;
 			getline(cin, input);
 			if (input.compare("west") == 0)
@@ -181,16 +170,9 @@ int main(int argc, char **argv) {
 			}
 			break;
 		case 9:system("Color FC");
-			picture9 = al_load_bitmap("Alucard.jpg");
-			al_draw_bitmap(picture9, 500, 200, 0);
-			al_flip_display();
-			al_destroy_bitmap(picture9);
 			cout << "You walk into a huge room. At the other end of it, you see a huge throne facing away from you, out a window. When the person in the throne gets out of the chair, you see that it is Boogie Barker. Before your eyes, he changes into a demon-like montser with a horn broken off. He calls himself Alucard." << endl;
-			if (health2 <= 0)
-				cout << "You kill alucard in his steps, and Corcraan is saved!" << endl;
 			break;
 		case 10:system("Color 2");
-			Monster();
 			cout << "You are in a room that is empty except for something hanging on the wall." << endl;
 			if (inv[6].compare("rocket launcher") != 0)
 				cout << "It's a rocket launcher" << endl;
@@ -206,14 +188,5 @@ int main(int argc, char **argv) {
 			}
 			break;
 		}
-	}
-}
-void Monster() {
-	int num = rand() % 100 + 1;
-	if (num >= 50) {
-		cout << "It's one of Boogie's goons!" << endl;
-	}
-	else {
-		cout << "You have escaped this room without being atacked" << endl;
 	}
 }
---
+++ Movie quote/Movie quote/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 0; i < 199; i++) {
+		cout << "They all float down here, Georgie" << endl;
+	}
+}
