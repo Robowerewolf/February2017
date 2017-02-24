8290c107c48a3ea1c225d980fe700b6735370bf1
Elias <ezwhalen64@gmaikl.com>
2017-02-24 12:41:54 -0700

    dgju

--- high number/high number/Source.cpp
+++ high number/high number/Source.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(void) {
-	int num; 
+	double num; 
 
-	for (num = 1; num <= 100000000; num = num * 10)
+	for (num = 1; num <= 1000000000; num = num * 10)
 		cout << num * 10 << endl;
 	return 0;
 }
